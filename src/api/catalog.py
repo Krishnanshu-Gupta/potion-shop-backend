3df263d5a43209f6bad8c78e658f7a0633a37170
Krishnanshu-Gupta <krishnanshu@gmail.com>
2023-10-02 16:36:04 -0700

    V1 catalog fix

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,12 +15,15 @@ def get_catalog():
     quantity = result.first().num_red_potions
 
     # Can return a max of 20 items.
-    return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": quantity,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
+    if quantity > 0:
+        return [
+                {
+                    "sku": "RED_POTION_0",
+                    "name": "red potion",
+                    "quantity": quantity,
+                    "price": 50,
+                    "potion_type": [100, 0, 0, 0],
+                }
         ]
+    else:
+        return []
